20dc4421169f5ac98a58c448047b9b4296127f1c
田中慎吾 <tanakashingo@mbp.local>
2020-02-18 13:56:18 +0900

    [Finish] 6-3

--- 6-3/app.js
+++ 6-3/app.js
@@ -1,70 +1,64 @@
-var alertString;
-alertString = addString("WebCamp");
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー")&&(user_hand != "チョキ")&&(user_hand != "パー")&&(user_hand != null)){
+	alert('グー•チョキ•パーのいずれかを入力して下さい');
+	user_hand = prompt('ジャンケンの手をグー、チョキ、パーから選んでください。');
+}
 
-alert(alertString);
+var js_hand = getJShand();
 
-function addString(strA){
-	var addStr = "Hello " + strA;
-	return addStr;
+var judge = winLose(user_hand, js_hand);
+
+if (user_hand != null){
+	alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+}else{
+	alert("またチャレンジしてね");
 }
 
 
+function getJShand(){
+    var js_hand_num = Math.floor( Math.random() * 3 );
+    var hand_name;
 
+    if(js_hand_num == 0){
+        hand_name = "グー";
+    }else if(js_hand_num == 1){
+        hand_name = "チョキ";
+    }else if(js_hand_num == 2){
+        hand_name = "パー";
+    }
+ 
+    return hand_name;
+}
+ 
 
-
-
-
-
-
-// var hello = "Hello, World";
-// alert(hello);
-
-// var int1 = 1;
-// alert(int1);
-
-// alert('Hello' + 'World');
-
-// var str1 = 'Hello';
-// var str2 = 'World!!';
-// alert(str1 + str2);
-
-
-// var orange = 100;
-// var apple = 120;
-
-// if(orange < apple){
-// 	alert('みかんの値段がりんごより安い');
-// }
-// else if (orange == apple){
-// 	alert('みかんとりんごが同じ値段');
-// }
-// else{
-// 	alert('みかんの値段がりんごより高い')
-// }
-
-// var max = 100;
-// var num = 1;
-// var count = 0;
-
-// while(num < max){
-// 	num = num * 2;
-// 	count = count + 1;
-// }
-
-// alert('2を掛け続けて'+ max +'を越えるのに必要だった回数は'+ count +'回です');
-
-// var i;
-// var num = 0;
-
-// for(i = 1;i < 11; i++){
-// 	num = num + i;
-// }
-
-// alert('1から10まで足し算した結果は' + num + 'です');
-
-// alert(1 + 1);
-// alert(2 - 2);
-// alert(3 * 3);
-// alert(4 / 4);
-
-
+function winLose(user, js){
+    var winLoseStr;
+ 
+    if(user == "グー"){
+        if(js == "グー"){
+            winLoseStr = "あいこ";
+        }else if(js == "チョキ"){
+            winLoseStr = "勝ち";
+        }else if(js == "パー"){
+            winLoseStr = "負け";
+        }
+    }else if(user == "チョキ"){
+        if(js == "グー"){
+            winLoseStr = "負け";
+        }else if(js == "チョキ"){
+            winLoseStr = "あいこ";
+        }else if(js == "パー"){
+            winLoseStr = "勝ち";
+        }
+    }else if(user == "パー"){
+        if(js == "グー"){
+            winLoseStr = "勝ち";
+        }else if(js == "チョキ"){
+            winLoseStr = "負け";
+        }else if(js == "パー"){
+            winLoseStr = "あいこ";
+        }
+    }
+ 
+    return winLoseStr;
+}
